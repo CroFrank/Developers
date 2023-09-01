cae97b9ce2b6fe99f4f51e2ca848967d1c45c6be
Fran <fducekuce@gmail.com>
2023-09-01 22:40:52 +0200

    another try

--- MERN/src/components/Test.tsx
+++ MERN/src/components/Test.tsx
@@ -6,7 +6,7 @@ export const Test = ({ dev }: { dev: Data }) => {
     const data = UseDashboardContext()
     const nameUser = data?.user.name
     const role = data?.user.role
-    if (nameUser === name || role === 'admin') {
+    if (nameUser === dev.name || role === 'admin') {
         return <>
             <Link to={`/dashboard/edit-dev/${dev._id}`} className="btn edit-btn">Edit</Link>
             <Form method="post" action={`../deletedev/${dev._id}`}>
